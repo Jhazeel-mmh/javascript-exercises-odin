39b1adc8ae5027eff7b1929b2e98c6e6abf5eb45
Jhazeel Hernandez <jhazeelhdz@gmail.com>
2024-06-22 17:01:07 -0600

    Add the reverseString function and tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    if (text === "") return text;
 
+    temp_text = [];
+    for (let c of text) {
+        temp_text.unshift(c);
+    }
+    
+    return temp_text.join("");
 };
 
 // Do not edit below this line
