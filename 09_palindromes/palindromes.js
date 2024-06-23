7ea480bed81beb88d49064f8b674aa37e6a52a7c
Jhazeel Hernandez <jhazeelhdz@gmail.com>
2024-06-23 12:44:07 -0600

    Add the palindromes function and test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    temp = []
+    tempTwo = []
+    for (let c of text.toLowerCase()){
+        let char = c.charCodeAt(0);
+        if ((char >= 32 && char <= 47) || (char >= 58 && char <= 64) ||
+            (char >= 91 && char <= 96)){
+            continue;
+        }
+        temp.unshift(c);
+        tempTwo.push(c);
+    }
+    return (temp.join("") === tempTwo.join(""));
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
