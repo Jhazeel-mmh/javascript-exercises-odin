f683ad81d7b9138d054ccb39b2b0f0e9cb9f0ced
Jhazeel Hernandez <jhazeelhdz@gmail.com>
2024-06-23 11:59:48 -0600

    Add the add, subtract, sum, multiply, power and factorial functions to calculator.js

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;    
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers = []) {
+  if (numbers === null) return 0;
+  let result = 0;
+  for (let num of numbers){
+    result += num;
+  }
+  return result;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers = []) {
+  if (numbers === null) return 0;
+  let result = 1;
+  for (let num of numbers){
+    result *= num;
+  }
+  return result;
 };
 
-const power = function() {
-	
+const power = function(number, power) {
+	return number ** power;
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+  if (number === 1 || number === 0) return 1;
+  let result = 1;
+  for (let i = number; i !== 1; i--){
+    result *= i; 
+  }
+  return result;
+
+	/*if (number === 1){
+    return 1;
+  }
+   
+  return number * factorial(number - 1);
+  */
 };
 
 // Do not edit below this line
