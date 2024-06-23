a7989413bfad52257d4f73ffe6e778007608d6c3
Jhazeel Hernandez <jhazeelhdz@gmail.com>
2024-06-22 18:02:48 -0600

    Add the removeFromArray function

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,34 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(myArray, ...targets) {
+    return myArray.filter( arrayelement => !targets.includes(arrayelement))
+}
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+/* older solution
+const removeFromArray = function(myArray, ...indexTarget) {
+    let count = 0;
+    if (myArray.length === indexTarget.length){
+        for (let i = 0; i < myArray.length; i++){
+            if (myArray[i] === indexTarget[i]){
+                count++;
+            }
+        }
+        if (count === myArray.length){
+            return [];
+        }
+    }
+
+    let target1 = indexTarget[0] || null,
+        target2 = indexTarget[1] || null,
+        result = [];
+
+    for (let i = 0; i < myArray.length; i++){
+        if (!(myArray[i] === target1 || myArray[i] === target2)){
+            result.push(myArray[i])
+        }
+    }
+    return result;
+};
+*/
