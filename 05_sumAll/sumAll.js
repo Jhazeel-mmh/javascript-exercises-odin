35bc3a911a39b43251ae68eb03717a8994e6c6a9
Jhazeel Hernandez <jhazeelhdz@gmail.com>
2024-06-23 10:17:20 -0600

    Add the sumAll function and pass the tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (typeof a  !== "number"|| a < 0) return "ERROR";
+    if (typeof b !== "number" || b < 0) return "ERROR";
 
+
+    let mayor, menor;
+    if (a > b) {
+        mayor = a;
+        menor = b;
+    } else {  
+        mayor = b;
+        menor = a;
+    }
+
+    let result = 0;
+    for (i = menor; i <= mayor; i++){
+        result += i;
+    }
+    return result;
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
+// FOR every number betwen a and b add the m to result (incluyendo a y b)
+//  while 1 < 4 
+//       1, 2, 3, 4
