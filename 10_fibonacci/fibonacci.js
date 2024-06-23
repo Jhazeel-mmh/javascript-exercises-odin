905d57862d43a3b70e47b92c25aa39f8264e966c
Jhazeel Hernandez <jhazeelhdz@gmail.com>
2024-06-23 13:50:19 -0600

    Ad the fibonacci function an tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(position) {
+    if (position == 0) return 0;
+    if (position < 0) return "OOPS";
+    sequ = [1, 1, 2, 3, 5, 8]
+    for (let i = 0; i <= position; i++){
+        if (sequ[i + 1] === undefined){
+            sequ.push(sequ[i] + sequ[i - 1]);
+        }
+    }
+    return sequ[position - 1];
 };
 
 // Do not edit below this line
