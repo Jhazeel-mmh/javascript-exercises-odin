efbea393a48f54500c311741ec5772b98771f75f
Jhazeel Hernandez <jhazeelhdz@gmail.com>
2024-06-28 14:59:44 -0600

    Add the findTheOldestFunction and testing

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(obj) {
+    let olderPerson =  obj.reduce((person1, person2) => {
+        let currentYear = new Date().getFullYear()
+        let agePerson1 = (person1.yearOfDeath || currentYear) - person1.yearOfBirth;
+        let agePerson2 = (person2.yearOfDeath || currentYear) - person2.yearOfBirth;
+        if (agePerson1 < agePerson2) return person2;
+        return person1;           
+    })
+    return olderPerson;
 };
 
 // Do not edit below this line
