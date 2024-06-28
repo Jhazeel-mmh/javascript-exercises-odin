5b7b8f35bb73ea4b1c4b8edf7ecde4da92e0264e
Jhazeel Hernandez <jhazeelhdz@gmail.com>
2024-06-28 14:36:28 -0600

    Add the getTheTitles function

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(object) {
+    let titles =  object.reduce((array, element) => {
+     array.push(element.title);
+     return array;
+    }, [])
+    return titles;
 };
 
 // Do not edit below this line
